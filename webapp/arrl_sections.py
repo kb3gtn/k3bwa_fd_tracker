ca6ad9ea10b76c511252e85c7d95546aba511e05
kb3gtn <kb3gtn@k3bwa-hub.k3bwa.us>
2026-05-16 15:08:29 -0400

    Corrected section errors, update display for section tracker

--- webapp/arrl_sections.py
+++ webapp/arrl_sections.py
@@ -13,6 +13,7 @@ SECTIONS = {
         ("ENY", "Eastern New York"), ("NLI", "New York City/Long Island"),
         ("NNJ", "Northern New Jersey"), ("NNY", "Northern New York"),
         ("SNJ", "Southern New Jersey"), ("WNY", "Western New York"),
+        ("VI", "US Virgin Islands (KP2)"),
     ],
     "W3": [
         ("DE", "Delaware"), ("EPA", "Eastern Pennsylvania"),
@@ -23,10 +24,11 @@ SECTIONS = {
         ("NC", "North Carolina"), ("NFL", "Northern Florida"),
         ("SC", "South Carolina"), ("SFL", "Southern Florida"),
         ("TN", "Tennessee"), ("VA", "Virginia"), ("WCF", "West Central Florida"),
+        ("PR", "Puerto Rico (KP4)"),
     ],
     "W5": [
         ("AR", "Arkansas"), ("LA", "Louisiana"), ("MS", "Mississippi"),
-        ("NM", "New Mexico"), ("NT", "North Texas"), ("OK", "Oklahoma"),
+        ("NM", "New Mexico"), ("NTX", "North Texas"), ("OK", "Oklahoma"),
         ("STX", "South Texas"), ("WTX", "West Texas"),
     ],
     "W6": [
@@ -34,10 +36,10 @@ SECTIONS = {
         ("SB", "Santa Barbara"), ("SCV", "Santa Clara Valley"),
         ("SDG", "San Diego"), ("SF", "San Francisco"),
         ("SJV", "San Joaquin Valley"), ("SV", "Sacramento Valley"),
-        ("PAC", "Pacific"),
+        ("PAC", "Pacific HI/Samoa/Guam (KH6)"),
     ],
     "W7": [
-        ("AK", "Alaska"), ("AZ", "Arizona"), ("EWA", "Eastern Washington"),
+        ("AK", "Alaska (KL7)"), ("AZ", "Arizona"), ("EWA", "Eastern Washington"),
         ("ID", "Idaho"), ("MT", "Montana"), ("NV", "Nevada"),
         ("OR", "Oregon"), ("UT", "Utah"), ("WWA", "Western Washington"),
         ("WY", "Wyoming"),
@@ -49,18 +51,23 @@ SECTIONS = {
         ("IL", "Illinois"), ("IN", "Indiana"), ("WI", "Wisconsin"),
     ],
     "VE": [
-        ("AB", "Alberta"), ("BC", "British Columbia"), ("MB", "Manitoba"),
-        ("NB", "New Brunswick"), ("NL", "Newfoundland/Labrador"),
-        ("NS", "Nova Scotia"), ("ONE", "Ontario East"), ("ONN", "Ontario North"),
-        ("ONS", "Ontario South"), ("PE", "Prince Edward Island"),
-        ("QC", "Quebec"), ("SK", "Saskatchewan"), ("TER", "Territories"),
+        ("AB", "Alberta (VA6/VE6)"), ("BC", "British Columbia (VA7/VE7)"), ("MB", "Manitoba (VA4/VE4)"),
+        ("NB", "New Brunswick (VE9)"), ("NL", "Newfoundland/Labrador (VO)"),
+        ("NS", "Nova Scotia (VA1/VE1)"), ("ONE", "Ontario East (VA3/VE3)"), ("ONN", "Ontario North (VE3/VA3)"),
+        ("ONS", "Ontario South (VA3/VE3)"), ("PE", "Prince Edward Island (VY2/CY9)"),
+        ("GTA", "Greater Toronto Area (VA3/VE3)"),
+        ("QC", "Quebec (VA2/VE2)"), ("SK", "Saskatchewan (VA4/VE5)"), ("TER", "Northern Territories (VY1/VE8/VY0)"),
+        ("MAR", "Canadian Maritime (VE0)"),
+        ("DX", "Not US or Canadia"),
     ],
 }
 
 BAND_NAMES = {
-    "1.8": "160m", "3.5": "80m", "7": "40m", "14": "20m",
-    "21": "15m", "28": "10m", "50": "6m", "144": "2m",
-    "222": "1.25m", "432": "70cm", "902": "33cm", "1240": "23cm",
+    "1": "160m", "1.8": "160m", "3": "80m", "3.5": "80m",
+    "5": "60m", "7": "40m", "10": "30m", "14": "20m",
+    "18": "17m", "21": "15m", "24": "12m", "28": "10m",
+    "50": "6m", "144": "2m", "222": "1.25m", "432": "70cm",
+    "902": "33cm", "1240": "23cm",
 }
 
 ALL_SECTIONS = [(abbr, name) for sections in SECTIONS.values() for abbr, name in sections]
